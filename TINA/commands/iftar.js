79444dc8c6627d7c1afd7cabe58c74224637a677
1rk823 <sumikhan8234@gmail.com>
2025-03-01 13:46:52 +0600

    Create iftar.js

---
+++ TINA/commands/iftar.js
@@ -0,0 +1,75 @@
+const axios = require("axios");
+const { getStreamFromURL } = global.utils;
+module.exports.config = {
+    name: "ramadan",
+    version: "1.0.0",
+    hasPermission: 0,
+    credits: "RAHAT",
+    description: "This command provides Ramadan timings information for a given city.",
+    usePrefix: true,
+    commandCategory: "Khan Rahul RK",
+    usages: "iftar Time",
+    cooldowns: 5,
+    dependencies: {
+    category: "Religion",
+    guide: { en:"{pn} district/state "},
+  },
+
+  module.exports.run: async function ({ api, args, message, event}) {
+    try {
+      if (args.length === 0) {
+        message.reply("Please provide a city/state name.");
+        return;
+      }
+
+      const botName = 'Halal Fox'; // add your bot name to show it in canvas image';
+
+      const cityName = args.join(" ");
+      message.reaction("⏰", event.messageID);
+      const apiUrl = `https://connect-foxapi.onrender.com/tools/ramadan?city=${encodeURIComponent(cityName)}&botName=${encodeURIComponent(botName)}`;
+      const response = await axios.get(apiUrl);
+
+      if (!response.data.city) {
+        message.reply("City not found. Please check the spelling [don't use Direct 'country' name, use your city or state name ]");
+        return;
+      }
+
+      const {
+        city,
+        hijriDate,
+        localTime,
+        today,
+        tomorrow,
+        canvas_img
+      } = response.data;
+
+      const ramadanInfo = "🌙 Ramadan Timings 🕌\n" +
+        "❏ City: " + city + "\n" +
+        "❏ Date: " + today.date + "\n" +
+        "❏ Hijri Date: " + hijriDate + "\n" +
+        "❏ Current Time: " + localTime + "\n\n" +
+        "Today's:\n" +
+        "❏ Sahr: " + today.sahr + "\n" +
+        "❏ Iftar: " + today.iftar + "\n\n" +
+        "Tomorrow:\n" +
+        "❏ Date: " + tomorrow.date + "\n" +
+        "❏ Sahr: " + tomorrow.sahr + "\n" +
+        "❏ Iftar: " + tomorrow.iftar + "\n\n" +
+        "❏ Note: 1 minute preventative difference in Sehri (-1 min) & Iftar (+1 min)";
+
+      const stream = await getStreamFromURL(canvas_img);
+
+      message.reply({
+        body: ramadanInfo,
+        attachment: stream
+      });
+      await message.reaction("✅", event.messageID);
+
+
+
+    } catch (error) {
+      console.error(error);
+      message.reply("City not found. Please check the spelling [don't use Direct 'country' name, use your city or state name ]");
+    }
+  }
+};
